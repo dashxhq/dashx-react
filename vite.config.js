47a2b9b8a00c0b3399abaf9a49f4380278a24707
Afzal Sayed <14029371+afzalsayed96@users.noreply.github.com>
2024-05-02 18:31:03 +0530

    Minor changes

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: [resolve(__dirname, "./src/index.ts")],
+      name: "ui-library",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: false,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
