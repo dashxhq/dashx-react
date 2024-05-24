5a53df9675f7b94cfb23eb9056757d8167a3a965
Amandeep Singh Ghai <Amandeepsinghghai@users.noreply.github.com>
2024-05-24 18:26:15 +0530

    Package css along with js to handle default styles. Create useInApp hook (#3)
    * Package css along with js to handle default styles
    
    * Improve styles
    
    * Cleanup handling server socket messages

--- src/components/NotificationBell.tsx
+++ src/components/NotificationBell.tsx
@@ -1,78 +1,58 @@
 import React, { useEffect, useState } from 'react'
-import toast from 'react-hot-toast'
-import useWebSocket from 'react-use-websocket'
-import { BellIcon } from '@heroicons/react/24/solid'
+import { BellIcon, EnvelopeIcon, EnvelopeOpenIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import { usePopper } from 'react-popper'
-import { WebsocketMessageType } from '@dashx/browser'
-import { XIcon } from '@heroicons/react/solid'
-import type { WebsocketMessage } from '@dashx/browser'
 
 import { useInApp } from '../hooks'
 
 const NotificationBell = () => {
-  let { client } = useInApp()
-  const [referenceElement, setReferenceElement] = useState<SVGSVGElement | null>(null)
   const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null)
+  const [referenceElement, setReferenceElement] = useState<SVGSVGElement | null>(null)
   const { styles, attributes } = usePopper(referenceElement, popperElement, {
     placement: 'bottom-end'
   })
   const [isAppInboxOpen, setIsAppInboxOpen] = useState(false)
-  const [connectWebsocket, setConnectWebsocket] = useState(false)
-
-  let { sendJsonMessage } = useWebSocket('ws://localhost:8082/websocket', {
-    queryParams: {
-      'publicKey': client.publicKey,
-      ...(client.targetEnvironment && { 'targetEnvironment': client.targetEnvironment })
-    },
-    shouldReconnect: (_) => true,
-    onError: errorEvent => console.error({ errorEvent }),
-    onClose: closeEvent => console.log({ closeEvent }),
-    onMessage: (messageEvent) => {
-      // @ts-ignore
-      let message: WebsocketMessage = {}
-      try {
-        message = JSON.parse(messageEvent.data)
-      } catch(e) {
-        console.error(e)
-      }
-
-      switch (message?.type) {
-        case WebsocketMessageType.SUBSCRIBE:
-          break;
-        case WebsocketMessageType.IN_APP_NOTIFICATION:
-          client.trackNotification({ id: message.data.notificationId, status: 'DELIVERED' })
-          toast(message.data.body)
-          break;
-        case WebsocketMessageType.INVALID:
-          console.error(message.data.message)
-          break;
-        default:
-          console.error('Unknown message type');
-      }
-    },
-    onOpen: (_) => {
-      let subscriptionMessage: WebsocketMessage = {
-        type: WebsocketMessageType.SUBSCRIBE,
-        data: {
-          accountUid: client.accountUid! // accountUid should exist by this point
-        }
-      }
-
-      sendJsonMessage(subscriptionMessage)
-    },
-  }, connectWebsocket);
+  let {
+    markNotificationAsRead,
+    markNotificationAsUnread,
+    notifications,
+    unreadNotificationsCount,
+    fetchInAppNotifications,
+  } = useInApp()
 
   useEffect(() => {
-    // Ensure the webook connection request is made ONLY once
-    setConnectWebsocket(true)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    if (isAppInboxOpen) {
+      fetchInAppNotifications()
+    }
+  }, [isAppInboxOpen])
+
+  const renderNotifications = () => {
+    if (notifications.length < 1) {
+      return <p className="text-black">No Notifications</p>
+    }
+
+    return notifications.map((notification) => {
+      let sentAt = new Date(notification.sentAt || new Date()).toLocaleString()
+
+      return (
+        <div className={`flex justify-between px-4 py-2 [&:not(:last-child)]:border-b-2 ${notification.readAt ? "bg-gray-500" : "bg-gray-700"}`} key={notification.id}>
+          <p>{notification.renderedContent.body}</p>
+          <div className="flex items-center">
+            <p className="pr-2">{sentAt}</p>
+            {notification.readAt
+              ? <EnvelopeOpenIcon className="h-4 w-4 cursor-pointer" onClick={() => markNotificationAsUnread(notification.id)} />
+              : <EnvelopeIcon className="h-4 w-4 cursor-pointer" onClick={() => markNotificationAsRead(notification.id)} />
+            }
+          </div>
+        </div>
+      )
+    })
+  }
 
   return (
     <React.Fragment>
       <BellIcon
         aria-hidden="true"
-        className="h-6 w-6"
+        className={`h-6 w-6 ${unreadNotificationsCount && 'text-red-600'}`}
         onClick={() =>  setIsAppInboxOpen(!isAppInboxOpen)}
         ref={setReferenceElement}
       />
@@ -82,12 +62,19 @@ const NotificationBell = () => {
           ref={setPopperElement}
           style={styles.popper}
           {...attributes.popper}
-          className="bg-gray-800 rounded-xl text-white w-96 h-96"
+          className="rounded-xl text-white cursor-default overflow-hidden"
         >
-          <div className="flex justify-between items-center border-y-2 border-white px-4 py-4">
-            <p>Notifications</p>
-            <XIcon height={20} width={20} onClick={() =>  setIsAppInboxOpen(false)} />
+          <div className="flex justify-between items-center border-y-2 border-white px-4 py-2 bg-gray-900 text-white">
+            <div className="flex items-center">
+              <p>Notifications</p>
+              {!!unreadNotificationsCount && <p className="pl-2 text-red-600">{unreadNotificationsCount}</p>}
+            </div>
+            <XMarkIcon height={20} width={20} className="cursor-pointer" onClick={() =>  setIsAppInboxOpen(false)} />
           </div>
+          <div className="bg-white h-96 w-96 overflow-scroll">
+            {renderNotifications()}
+          </div>
+          <p className="bold px-4 py-2 bg-gray-900">Powered by DashX</p>
         </div>
       )}
     </React.Fragment>
--- src/hooks/use-in-app.ts
+++ src/hooks/use-in-app.ts
@@ -1,24 +1,98 @@
 import { useEffect, useState } from "react"
-import type { Client, InAppNotificationRecipient } from '@dashx/browser'
+import toast from 'react-hot-toast'
+import useWebSocket from 'react-use-websocket'
+import { WebsocketMessage } from '@dashx/browser'
+import type { InAppNotifications, WebsocketMessageType } from '@dashx/browser'
 
 import useDashXProvider from "./use-dashx-provider"
 
 type UseInAppHookResponse = {
-  client: Client,
-  notifications: InAppNotificationRecipient[],
+  notifications: InAppNotifications,
+  unreadNotificationsCount: number | null,
+  fetchInAppNotifications: () => void,
+  markNotificationAsRead: (id: string) => Promise<any>,
+  markNotificationAsUnread: (id: string) => Promise<any>,
 }
+
+const DASHX_CLOSE_CODES = [
+  40000,
+  40001,
+  50000
+]
 
 const useInApp = (): UseInAppHookResponse => {
   let dashX = useDashXProvider()
-  const [ notifications, setNotifications ] = useState<InAppNotificationRecipient[]>([])
+  const [connectWebsocket, setConnectWebsocket] = useState(false)
+  const [notifications, setNotifications] = useState<InAppNotifications>([])
+  const [unreadNotificationsCount, setUnreadNotificationsCount] = useState<number | null>(null)
+
+  const markNotificationAsRead = (id: string) => dashX.trackNotification({ id, status: 'READ' })
+
+  const markNotificationAsUnread = async (id: string) => dashX.trackNotification({ id, status: 'UNREAD' })
+
+  const fetchInAppNotifications = () => dashX.fetchInAppNotifications()
+
+  useEffect(() =>{
+    dashX.watchFetchInAppNotifications(setNotifications)
+    dashX.watchFetchInAppNotificationsAggregate(setUnreadNotificationsCount)
+  }, [])
+
+  let { sendJsonMessage } = useWebSocket('ws://localhost:9999', {
+    queryParams: {
+      publicKey: dashX.publicKey,
+      ...(dashX.identityToken && { 'identityToken': dashX.identityToken }),
+      ...(dashX.targetEnvironment && { 'targetEnvironment': dashX.targetEnvironment })
+    },
+    shouldReconnect: (closeEvent) => {
+      if (DASHX_CLOSE_CODES.includes(closeEvent.code)) {
+        return false
+      }
+
+      return true
+    },
+    onError: errorEvent => console.error({ errorEvent }),
+    onClose: closeEvent => console.log({ closeEvent }),
+    onMessage: async (messageEvent) => {
+      let message:WebsocketMessageType = JSON.parse(messageEvent.data)
+
+      switch (message?.type) {
+        case WebsocketMessage.SUBSCRIBE:
+          break;
+        case WebsocketMessage.IN_APP_NOTIFICATION:
+          dashX.trackNotification({ id: message.data.id, status: 'DELIVERED' })
+          dashX.addInAppNotificationToCache(message.data)
+          toast(message.data.renderedContent.body)
+          break;
+        case WebsocketMessage.SUBSCRIPTION_SUCCEEDED:
+          fetchInAppNotifications()
+          break;
+        default:
+          throw new Error(`Unknown message type ${message}`);
+      }
+    },
+    onOpen: (_) => {
+      let subscriptionMessage: WebsocketMessageType = {
+        type: WebsocketMessage.SUBSCRIBE,
+        data: {
+          accountUid: dashX.accountUid! // accountUid should exist by this point
+        }
+      }
+
+      sendJsonMessage(subscriptionMessage)
+    },
+  }, connectWebsocket);
 
   useEffect(() => {
-    dashX.listInAppNotifications()
-      .then((response) => setNotifications(response))
-      .catch((err)=> console.error(err))
-  }, [dashX]);
+    setConnectWebsocket(true) // Ensure the webook connection request is made ONLY once
+  }, [])
 
-  return { client: dashX, notifications }
+  return {
+    notifications,
+    unreadNotificationsCount,
+    fetchInAppNotifications,
+    markNotificationAsRead,
+    markNotificationAsUnread
+  }
 }
 
 export default useInApp
